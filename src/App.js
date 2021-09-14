7b58a51debb72b241cda105c0d71c4e307c1fdb7
bitanSarkar <bitansarkar12345@gmail.com>
2021-09-14 13:42:42 +0530

    feat: added image and description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.css";
+import bitan from './images/myPhoto.png';
+import { description, firstname, lastname } from './constants';
 const App = () =>  {
   return (
     <div className="App">
-      
+      <div className="jumbo_header">
+        <div className="my_image_row">
+          <img src={bitan} alt={firstname} className="my_image"/>
+        </div>
+        <div className="details_row">
+          <div className="name">{firstname} {lastname}</div>
+          <div className="description">{description}</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const firstname = "Bitan";
+export const lastname = "Sarkar";
+export const description = "Full Stack Java Developer & React Developer";
